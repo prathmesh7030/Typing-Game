e7defef3987e5bf66372701e876993d0185827fb
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-08-14 22:01:54 +0530

    Set difficulty

--- Logic.js
+++ Logic.js
@@ -40,3 +40,15 @@ let score = 0;
 
 // Init time
 let time = 10;
+
+// Set difficulty to value in ls or medium
+let difficulty =
+  localStorage.getItem('difficulty') !== null
+    ? localStorage.getItem('difficulty')
+    : 'medium';
+
+// Set difficulty select value
+difficultySelect.value =
+  localStorage.getItem('difficulty') !== null
+    ? localStorage.getItem('difficulty')
+    : 'medium';
